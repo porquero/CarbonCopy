3d2b727c6d8d92171becf006b7d61c24c2b0214c
porquero <criffoh@gmail.com>
2015-07-30 17:39:44 -0300

    * Added config to manage email notifications

--- application/modules/account/controllers/manage.php
+++ application/modules/account/controllers/manage.php
@@ -1,6 +1,6 @@
 <?php
 
-if (!defined('BASEPATH'))
+if ( ! defined('BASEPATH'))
     exit('No direct script access allowed');
 
 /**
@@ -17,7 +17,8 @@ class manage extends MX_Controller {
      *
      * @param string $username first account participant
      */
-    public function create($username) {
+    public function create($username)
+    {
         $this->load->module('file/write');
         $this->load->model('m_user');
         $this->load->model('cc/m_timeline');
@@ -59,7 +60,8 @@ class manage extends MX_Controller {
                 'accounts' => $account_id
             );
             $this->m_user->update($username, $data);
-        } else {
+        }
+        else {
             Modules::run('cc/user/add_account', $username, $account_id);
         }
     }
@@ -69,7 +71,8 @@ class manage extends MX_Controller {
      *
      * @param string $id_account
      */
-    public function set_current($id_account) {
+    public function set_current($id_account)
+    {
         $this->load->model('cc/m_user');
         if ($this->m_user->has_account(connected_user(), $id_account)) {
             $this->session->set_userdata('current_account', $id_account);
@@ -82,7 +85,8 @@ class manage extends MX_Controller {
     /**
      * Account config form
      */
-    public function config_form() {
+    public function config_form()
+    {
         is_connected('administrator');
 
         $this->load->helper('form');
@@ -91,26 +95,37 @@ class manage extends MX_Controller {
         $account_info = Modules::run('file/read/json_content', "_accounts/{$this->session->userdata('current_account')}/info_account.json");
 
         $this->tpl->variables(
-                array(
-                    'title' => 'Account config',
-                    'description' => '',
-                    'account_date_format' => $account_config['date_format'],
-                    'account_language' => $account_config['language'],
-                    'context_label' => $account_config['context_label'],
-                    'topic_label' => $account_config['topic_label'],
-                    'account_name' => $account_info['name'],
-                    'account_info' => $account_info['info'],
-                    'footer' => js_tag('pub/js/jquery.form.js') . js_tag('pub/js/nicedit/nicEdit.js') . js_tag('pub/web_tpl/js/account_config.js'),
-                    'date_format' => array(
-                        'd-m-Y' => 'dd-mm-yyyy',
-                        'Y-m-d' => 'yyyy-mm-dd'
-                    ),
-                    'language' => array(
-                        'english' => 'english',
-                        'spanish' => 'spanish',
-                    ),
-                    'msg_type' => isset($msg_type) ? $msg_type : '',
-                    'msg' => isset($msg) ? $msg : '',
+          array(
+              'title' => 'Account config',
+              'description' => '',
+              'account_date_format' => $account_config['date_format'],
+              'account_language' => $account_config['language'],
+              'context_label' => $account_config['context_label'],
+              'topic_label' => $account_config['topic_label'],
+              'account_name' => $account_info['name'],
+              'account_info' => $account_info['info'],
+              'footer' => js_tag('pub/js/jquery.form.js') . js_tag('pub/js/nicedit/nicEdit.js') . js_tag('pub/web_tpl/js/account_config.js'),
+              'date_format' => array(
+                  'd-m-Y' => 'dd-mm-yyyy',
+                  'Y-m-d' => 'yyyy-mm-dd'
+              ),
+              'language' => array(
+                  'english' => 'english',
+                  'spanish' => 'spanish',
+              ),
+              'msg_type' => isset($msg_type) ? $msg_type : '',
+              'msg' => isset($msg) ? $msg : '',
+              'notification' => (string) $account_config['notification'],
+              'notification_day' => $account_config['notification_day'],
+              'week_days' => array(
+                  'monday' => lang('monday'),
+                  'tuesday' => lang('tuesday'),
+                  'wednesday' => lang('wednesday'),
+                  'thursday' => lang('thursday'),
+                  'friday' => lang('friday'),
+                  'saturday' => lang('saturday'),
+                  'sunday' => lang('sunday'),
+              ),
         ));
 
         $this->tpl->section('_sidebar', '_sidebar.phtml');
@@ -123,7 +138,8 @@ class manage extends MX_Controller {
      *
      * @return string
      */
-    public function config() {
+    public function config()
+    {
         is_connected('administrator');
 
         $this->load->module('file/misc');
@@ -135,7 +151,8 @@ class manage extends MX_Controller {
             if ($this->input->is_ajax_request()) {
                 echo json_encode($result);
                 return;
-            } else {
+            }
+            else {
                 return $result;
             }
         }
@@ -150,7 +167,9 @@ class manage extends MX_Controller {
                 'language' => $this->input->post('language'),
                 'context_label' => $this->input->post('context_label'),
                 'topic_label' => $this->input->post('topic_label'),
-            ) + Modules::run('account/configuration/load');
+                'notification' => $this->input->post('notification'),
+                'notification_day' => $this->input->post('week_day'),
+              ) + Modules::run('account/configuration/load');
 
             $config_saved = $this->write->archive("_accounts/{$this->session->userdata('current_account')}/config.json", json_encode($account_conifg));
 
@@ -169,7 +188,8 @@ class manage extends MX_Controller {
             if ($res == 'fail') {
                 //@todo Error number is manual now. Create secuencial system error codes next.
                 $message = 'Sytem error number #7732143. Please contact us and send the error number.';
-            } else {
+            }
+            else {
                 $this->session->set_flashdata('msg', lang('config_saved'));
                 $this->session->set_flashdata('msg_type', 'msg_ok');
             }
@@ -178,7 +198,8 @@ class manage extends MX_Controller {
                 'result' => $res,
                 'message' => $message,
             );
-        } else {
+        }
+        else {
             $message = validation_errors();
             $result = array(
                 'result' => 'fail',
@@ -188,7 +209,8 @@ class manage extends MX_Controller {
 
         if ($this->input->is_ajax_request()) {
             echo json_encode($result);
-        } else {
+        }
+        else {
             return $result;
         }
     }
@@ -198,7 +220,8 @@ class manage extends MX_Controller {
      *
      * @return array
      */
-    public function validate_form() {
+    public function validate_form()
+    {
         $this->load->library('form_validation');
         $this->load->helper(array('form'));
         $this->load->language('cc_form_validation');
@@ -239,7 +262,8 @@ class manage extends MX_Controller {
      *
      * @return array
      */
-    private function _date_format($type) {
+    private function _date_format($type)
+    {
         $types = array(
             'd-m-Y' => array(
                 'date_format' => 'd-m-Y',
@@ -259,18 +283,19 @@ class manage extends MX_Controller {
     /**
      * Extends configuration
      */
-    public function extends_form() {
+    public function extends_form()
+    {
         is_connected('administrator');
         $this->tpl->variables(
-                array(
-                    'title' => lang('extensions_manager'),
-                    'description' => '',
-                    'footer' => '',
-                    'msg_type' => isset($msg_type) ? $msg_type : '',
-                    'msg' => isset($msg) ? $msg : '',
-                    'components' => Modules::run('extends/component/installed'),
-                    'sections' => Modules::run('extends/section/installed'),
-                    'footer' => js_tag('pub/web_tpl/js/account_config.js'),
+          array(
+              'title' => lang('extensions_manager'),
+              'description' => '',
+              'footer' => '',
+              'msg_type' => isset($msg_type) ? $msg_type : '',
+              'msg' => isset($msg) ? $msg : '',
+              'components' => Modules::run('extends/component/installed'),
+              'sections' => Modules::run('extends/section/installed'),
+              'footer' => js_tag('pub/web_tpl/js/account_config.js'),
         ));
 
         $this->tpl->section('_sidebar', '_sidebar.phtml');
@@ -281,17 +306,18 @@ class manage extends MX_Controller {
     /**
      * Account config principal view.
      */
-    public function index() {
+    public function index()
+    {
 
         $components = '';
 
         $this->tpl->variables(
-                array(
-                    'title' => lang('account_configuration'),
-                    'description' => '',
-                    'footer' => '',
-                    'msg_type' => isset($msg_type) ? $msg_type : '',
-                    'msg' => isset($msg) ? $msg : '',
+          array(
+              'title' => lang('account_configuration'),
+              'description' => '',
+              'footer' => '',
+              'msg_type' => isset($msg_type) ? $msg_type : '',
+              'msg' => isset($msg) ? $msg : '',
         ));
 
         $this->tpl->section('_view', 'index.phtml');
